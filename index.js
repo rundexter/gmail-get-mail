f5e900c06eb4991d36a2f219506f5e0d09cb6e73
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-24 17:35:38 +0200

    refactore

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ module.exports = {
             }
         }, this);
 
-        if(!dexter.environment('access_token')) {
+        if(!dexter.environment('google_access_token')) {
 
-            this.fail('A access_code environment variable is required for this module');
+            this.fail('A google_access_code environment variable is required for this module');
         }
     },
 
@@ -41,7 +41,7 @@ module.exports = {
         this.checkAuthOptions(step, dexter);
 
         var oauth2Client = new OAuth2();
-        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+        oauth2Client.setCredentials({access_token: dexter.environment('google_access_token'), refresh_token: dexter.environment('google_refresh_token')});
 
         google.options({ auth: oauth2Client });
         google.gmail('v1').users.messages.get({auth: oauth2Client, id: step.input('id', null).first(), userId: step.input('userId', null).first()}, function (err, message) {
