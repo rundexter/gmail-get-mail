c393abd1e43fedb4eee77f3fdaf18da068fd927f
Mike Friedman <friedo@friedo.com>
2016-02-23 16:45:17 -0500

    debugging

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ var fetch_msg = function( app, service, user, msg_id ) {
     app.log( 'constructing promise for msg id ' + msg_id );
     var deferred = Q.defer();
     service.users.messages.get( { 'id': msg_id, 'userId': user }, function( err, message ) {
-        if ( err ) deferred.reject( err );
-        else       deferred.resolve( message );
+        if ( err ) { deferred.reject( err );      }
+        else       { deferred.resolve( message ); }
     } );
 
     return deferred.promise;
